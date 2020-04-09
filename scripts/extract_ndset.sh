925ecc93eeb8015f1a9a2795c89912e102a8be46
Jumperkables <tom.winterbottom@msn.com>
2020-04-09 03:10:38 +0100

    works

--- scripts/extract_ndset.sh
+++ scripts/extract_ndset.sh
@@ -2,8 +2,8 @@
 cd "$(dirname "$0")"
 source /home/jumperkables/kable_management/python_venvs/vm/bin/activate
 python ../main.py \
-    --raw_frame_rootdir /home/jumperkables/kable_management/data/visual_modelling/dataset_v1.0/raw \
-    --extracted_n_dset_savepathdir /home/jumperkables/kable_management/data/visual_modelling/dataset_v1.0 \
+    --raw_frame_rootdir /home/crhf63/kable_management/data/visual_modelling/dataset_v1.0/raw \
+    --extracted_n_dset_savepathdir /home/crhf63/kable_management/data/visual_modelling/dataset_v1.0 \
     --in_no 10 \
     --out_no 1 \
     --extract_n_dset_file 
