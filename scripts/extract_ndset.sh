6abada3ac7b5f8bed5d1c6cae6cab6e09f787cca
Jumperkables <tom.winterbottom@msn.com>
2020-06-17 20:39:45 +0100

    5-1 runs

--- scripts/extract_ndset.sh
+++ scripts/extract_ndset.sh
@@ -4,7 +4,7 @@ source /home2/crhf63/kable_management/python_venvs/vm/bin/activate
 python ../main.py \
     --raw_frame_rootdir data/hudsons_og/2000 \
     --extracted_n_dset_savepathdir data/hudsons_og/2000 \
-    --in_no 99 \
+    --in_no 5 \
     --out_no 1 \
     --extract_n_dset_file 
 
