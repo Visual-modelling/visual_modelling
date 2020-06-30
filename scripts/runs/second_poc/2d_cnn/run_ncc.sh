0b3eb1db0131fb75e172091320127ef040fd8e37
Thomas Winterbottom <crhf63@ncc1.clients.dur.ac.uk>
2020-06-30 23:26:11 +0100

    editing ncc scripts

--- scripts/runs/second_poc/2d_cnn/run_ncc.sh
+++ scripts/runs/second_poc/2d_cnn/run_ncc.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-sbatch 5-1_blurred/sl1.sh
-sbatch 5-1_blurred/SSIM.sh
-sbatch 5-1_masked/sl1.sh
-sbatch 5-1_masked/SSIM.sh
-sbatch 5-1_masked_blurred/sl1.sh
-sbatch 5-1_masked_blurred/SSIM.sh
+cd /home2/crhf63/kable_management/projects/Visual-modelling/cnn_visual_modelling/scripts/runs/second_poc/2d_cnn/5-1_blurred
+sbatch sl1.sh
+sbatch SSIM.sh
+cd /home2/crhf63/kable_management/projects/Visual-modelling/cnn_visual_modelling/scripts/runs/second_poc/2d_cnn/5-1_masked
+sbatch sl1.sh
+sbatch SSIM.sh
+cd /home2/crhf63/kable_management/projects/Visual-modelling/cnn_visual_modelling/scripts/runs/second_poc/2d_cnn/5-1_masked_blurred
+sbatch sl1.sh
+sbatch SSIM.sh
