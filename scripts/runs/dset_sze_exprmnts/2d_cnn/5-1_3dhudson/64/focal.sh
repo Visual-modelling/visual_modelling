141c10bb4f32a6b58d8fd8dce85cc3dd13997499
Jumperkables <tom.winterbottom@msn.com>
2020-08-26 05:54:40 +0100

    modelling updates

---
+++ scripts/runs/dset_sze_exprmnts/2d_cnn/5-1_3dhudson/64/focal.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --qos short
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH -t 2-00:00
+#SBATCH -x gpu[0-3]
+#SBATCH --mem 12G
+#SBATCH -p res-gpu-small
+#SBATCH --job-name varysize-64_3dhudson_2d-d3_focal-mean
+#SBATCH --gres gpu:1
+#SBATCH -o /home2/crhf63/kable_management/projects/Visual-modelling/cnn_visual_modelling/.results/varysize-64_3dhudson_2d-d3_focal-mean.out
+
+source /home2/crhf63/kable_management/python_venvs/vm/bin/activate
+python ../../../../../../main.py \
+    --dataset_path data/3d_hudson/6_dset.pickle \
+    --bsz 16 \
+    --val_bsz 100 \
+    --in_no 5 \
+    --out_no 1 \
+    --depth 3 \
+    --train_ratio 0.8 \
+    --device 0 \
+    --epoch 300 \
+    --early_stopping 100 \
+    --n_gifs 16 \
+    --self_output_n 100 \
+    --jobname jmprkbls_varysize-64_5-1_3dhudson_2d-d3_focal_mean \
+    --loss focal \
+    --reduction mean \
+    --img_type greyscale \
+    --model UpDown2D \
+    --self_output \
+    --save \
+    --shuffle \
+    --visdom \
+    --reduce \
+    --dset_sze 64 
