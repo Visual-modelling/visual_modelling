0fa6b8abea112908bb56c9466163b28f115210a1
Jumperkables <tom.winterbottom@msn.com>
2021-07-02 20:39:14 +0100

    extra batch size jobs with lr lowered

--- scripts/runs/modelling/CNN/2dBouncing/Comparison_bsz-4_lr-1e-6_2dBouncingMG-y_5-1_k-3_d-3_ssim.sh
+++ scripts/runs/modelling/CNN/2dBouncing/Comparison_bsz-4_lr-1e-6_2dBouncingMG-y_5-1_k-3_d-3_ssim.sh
@@ -5,9 +5,9 @@
 #SBATCH -t 2-00:00
 #SBATCH --mem 12G
 #SBATCH -p res-gpu-small
-#SBATCH --job-name Comparison_bsz-4_2dBouncingMG-y_5-1_k-3_d-3_sl1-mean 
+#SBATCH --job-name Comparison_bsz-4_lr-1e-6_2dBouncingMG-y_5-1_k-3_d-3_ssim 
 #SBATCH --gres gpu:1
-#SBATCH -o ../../../../../.results/Comparison_bsz-4_2dBouncingMG-y_5-1_k-3_d-3_sl1-mean.out
+#SBATCH -o ../../../../../.results/Comparison_bsz-4_lr-1e-6_2dBouncingMG-y_5-1_k-3_d-3_ssim.out
 cd ../../../../..
 source python_venvs/vm/bin/activate
 export PYTHONBREAKPOINT=ipdb.set_trace
@@ -17,8 +17,8 @@ python VM_train.py \
     --dataset_path data/2dBouncing/2dMultiGrav-Y_regen/raw \
     --split_condition tv_ratio:4-1 \
     --bsz 4 \
-    --lr 1e-5 \
     --val_bsz 100 \
+    --lr 1e-6 \
     --num_workers 4 \
     --in_no 5 \
     --out_no 1 \
@@ -28,8 +28,8 @@ python VM_train.py \
     --device 0 \
     --epoch 75 \
     --n_gifs 12 \
-    --jobname Comparison_bsz-4_2dBouncingMG-y_5-1_k-3_d-3_sl1-mean \
-    --loss sl1 \
+    --jobname Comparison_bsz-4_lr-1e-6_2dBouncingMG-y_5-1_k-3_d-3_ssim \
+    --loss ssim \
     --reduction mean \
     --img_type greyscale \
     --model UpDown2D \
