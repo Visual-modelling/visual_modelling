c0e2892c5f4bd1d41e0c1313879ae8a1d6e7aead
Daniel Kl <accounts@kluvanec.com>
2021-07-19 11:24:06 +0100

    Updated Scripts

--- scripts/runs/tasks/image_transformer/template.sh
+++ scripts/runs/tasks/image_transformer/template.sh
@@ -5,9 +5,9 @@
 #SBATCH -t 2-00:00
 #SBATCH --mem 28G
 #SBATCH -p res-gpu-small
-#SBATCH --job-name mutual_attract_transformer_lr1e-5_sl1_018
+#SBATCH --job-name TODO
 #SBATCH --gres gpu:1
-#SBATCH -o ../../../../../.results/mutual_attract_transformer_lr1e-5_sl1_018.out
+#SBATCH -o ../../../../../.results/TODO.out
 cd ../../../../..
 source python_venvs/vm/bin/activate
 export PYTHONBREAKPOINT=ipdb.set_trace
@@ -17,7 +17,7 @@ python test_tasks.py \
     --model_path TODO\
     --linear_probes \
     --encoder_freeze \
-    --jobname mutual_attract_transformer_lr1e-5_sl1_018 \
+    --jobname TODO \
     --dataset simulations \
     --split_condition tv_ratio:8-1-1 \
     --bsz 64 \
