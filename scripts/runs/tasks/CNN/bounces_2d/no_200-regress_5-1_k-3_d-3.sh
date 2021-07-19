760cffcf3bcf4b14ae4101437203ab1fecfc7a31
Jumperkables <tom.winterbottom@msn.com>
2021-07-19 16:13:29 +0100

    bouncing re-runs to NCC

--- scripts/runs/tasks/CNN/bounces_2d/no_200-regress_5-1_k-3_d-3.sh
+++ scripts/runs/tasks/CNN/bounces_2d/no_200-regress_5-1_k-3_d-3.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-#SBATCH --ntasks 6
-#SBATCH -p part0
+#SBATCH --qos short
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH -t 2-00:00
+#SBATCH --mem 20G
+#SBATCH -p res-gpu-small
 #SBATCH --job-name no_200_bounces-regress_2d_5-1_k-3_d-3  
 #SBATCH --gres gpu:1
 #SBATCH -o ../../../../../.results/no_200_bounces-regress_2d_5-1_k-3_d-3.out
