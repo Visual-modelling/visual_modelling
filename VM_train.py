fda11bf9f9de1f17ab7c1977efeba5c4b965b528
Daniel Kl <accounts@kluvanec.com>
2021-07-19 22:08:58 +0100

    Moved import statement for VM_MixSeg to body of code where it's used to prevent errors when it's packages are not installed, even if the model is not used.

--- VM_train.py
+++ VM_train.py
@@ -34,8 +34,8 @@ from models.UpDown2D import FCUpDown2D
 from models.transformer import ImageTransformer
 from models.deans_transformer import VMDecoder as DeansTransformer
 
-# From 
-from models.patch_transformer import VM_MixSeg
+# From
+# from models.patch_transformer import VM_MixSeg  # moved this to the body of the code where it's used
 #from models.mmseg.models.backbones.mix_transformer import MixVisionTransformer
 
 ################################################################################
@@ -272,6 +272,7 @@ class ModellingSystem(pl.LightningModule):
         elif args.model == "deans_transformer":
             self.model = DeansTransformer(in_dim=args.d_model, layers=args.n_layers, heads=args.nhead)
         elif args.model == "PatchTrans":
+            from models.patch_transformer import VM_MixSeg
             self.model = VM_MixSeg(img_size=64, in_chans=args.in_no, out_chans=args.out_no)
         else:
             raise ValueError(f"Unknown model: {args.model}")
