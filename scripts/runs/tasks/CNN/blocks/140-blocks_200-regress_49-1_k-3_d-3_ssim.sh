252d638524c8c958045a6b3acf66d08ca76e74bc
Jumperkables <tom.winterbottom@msn.com>
2021-07-23 20:30:58 +0100

    final script to run is ready

--- scripts/runs/tasks/CNN/blocks/140-blocks_200-regress_49-1_k-3_d-3_ssim.sh
+++ scripts/runs/tasks/CNN/blocks/140-blocks_200-regress_49-1_k-3_d-3_ssim.sh
@@ -5,9 +5,9 @@
 #SBATCH -t 2-00:00
 #SBATCH --mem 21G
 #SBATCH -p res-gpu-small
-#SBATCH --job-name XX-blocks_200_blocks-regress_49-1_k-3_d-3_ssim  
+#SBATCH --job-name 140-blocks_200_blocks-regress_49-1_k-3_d-3_ssim  
 #SBATCH --gres gpu:1
-#SBATCH -o ../../../../../.results/XX-blocks_200_blocks-regress_49-1_k-3_d-3_ssim.out
+#SBATCH -o ../../../../../.results/140-blocks_200_blocks-regress_49-1_k-3_d-3_ssim.out
 cd ../../../../..
 export PYTHONBREAKPOINT=ipdb.set_trace
 source python_venvs/vm/bin/activate
@@ -26,10 +26,10 @@ python test_tasks.py \
     --device 0 \
     --lr 1e-4 \
     --epoch 200 \
-    --jobname XX-blocks_200_blocks-regress_49-1_k-3_d-3_ssim \
+    --jobname 140-blocks_200_blocks-regress_49-1_k-3_d-3_ssim \
     --img_type greyscale \
     --model UpDown2D \
-    --model_path '.results/' \
+    --model_path '.results/blocks_49-1_k-3_d-3_lr-1e-4_ssim-epoch=140.ckpt' \
     --encoder_freeze \
     --linear_probes \
     --shuffle \
