5c500b8109a78767ac11b8e74f24e162194ab91d
Jumperkables <tom.winterbottom@msn.com>
2021-07-25 17:35:16 +0100

    pt transformer to NCC

--- scripts/runs/modelling/patch_transformer/blocks/blocks_49-1_k-3_d-3_lr-1e-4_ssim.sh
+++ scripts/runs/modelling/patch_transformer/blocks/blocks_49-1_k-3_d-3_lr-1e-4_ssim.sh
@@ -5,31 +5,31 @@
 #SBATCH -t 2-00:00
 #SBATCH --mem 21G
 #SBATCH -p res-gpu-small
-#SBATCH --job-name pt_moon_5-1_k-3_d-3_lr-1e-5_ssim 
+#SBATCH --job-name pt_blocks_49-1_k-3_d-3_lr-1e-4_ssim 
 #SBATCH --gres gpu:1
-#SBATCH -o ../../../../../.results/pt_moon_5-1_k-3_d-3_lr-1e-5_ssim.out
+#SBATCH -o ../../../../../.results/pt_blocks_49-1_k-3_d-3_lr-1e-4_ssim.out
 cd ../../../../..
 source python_venvs/vm/bin/activate
-export MASTER_PORT=10019
+export MASTER_PORT=10010
 export PYTHONBREAKPOINT=ipdb.set_trace
 # Pretrain
 python VM_train.py \
     --dataset simulations \
-    --dataset_path data/myphysicslab/Moon_10000 \
+    --dataset_path data/myphysicslab/Blocks_10000 \
     --split_condition tv_ratio:8-1-1 \
     --bsz 64 \
     --val_bsz 100 \
     --num_workers 1 \
-    --in_no 5 \
+    --in_no 49 \
     --out_no 1 \
     --depth 3 \
     --krnl_size 3 \
     --padding 1 \
     --device 0 \
-    --lr 1e-5 \
+    --lr 1e-4 \
     --epoch 150 \
     --n_gifs 20 \
-    --jobname pt_moon_5-1_k-3_d-3_lr-1e-5_ssim \
+    --jobname pt_blocks_49-1_k-3_d-3_lr-1e-4_ssim \
     --loss ssim \
     --reduction mean \
     --img_type greyscale \
--- scripts/runs/modelling/patch_transformer/moon/moon_5-1_k-3_d-3_lr-1e-4_sl1-mean.sh
+++ scripts/runs/modelling/patch_transformer/moon/moon_5-1_k-3_d-3_lr-1e-4_sl1-mean.sh
@@ -5,31 +5,31 @@
 #SBATCH -t 2-00:00
 #SBATCH --mem 21G
 #SBATCH -p res-gpu-small
-#SBATCH --job-name pt_blocks_49-1_k-3_d-3_lr-1e-5_sl1-mean 
+#SBATCH --job-name pt_moon_5-1_k-3_d-3_lr-1e-4_sl1-mean 
 #SBATCH --gres gpu:1
-#SBATCH -o ../../../../../.results/pt_blocks_49-1_k-3_d-3_lr-1e-5_sl1-mean.out
+#SBATCH -o ../../../../../.results/pt_moon_5-1_k-3_d-3_lr-1e-4_sl1-mean.out
 cd ../../../../..
 source python_venvs/vm/bin/activate
-export MASTER_PORT=10009
+export MASTER_PORT=10018
 export PYTHONBREAKPOINT=ipdb.set_trace
 # Pretrain
 python VM_train.py \
     --dataset simulations \
-    --dataset_path data/myphysicslab/Blocks_10000 \
+    --dataset_path data/myphysicslab/Moon_10000 \
     --split_condition tv_ratio:8-1-1 \
     --bsz 64 \
     --val_bsz 100 \
     --num_workers 1 \
-    --in_no 49 \
+    --in_no 5 \
     --out_no 1 \
     --depth 3 \
     --krnl_size 3 \
     --padding 1 \
     --device 0 \
-    --lr 1e-5 \
+    --lr 1e-4 \
     --epoch 150 \
     --n_gifs 20 \
-    --jobname pt_blocks_49-1_k-3_d-3_lr-1e-5_sl1-mean \
+    --jobname pt_moon_5-1_k-3_d-3_lr-1e-4_sl1-mean \
     --loss sl1 \
     --reduction mean \
     --img_type greyscale \
