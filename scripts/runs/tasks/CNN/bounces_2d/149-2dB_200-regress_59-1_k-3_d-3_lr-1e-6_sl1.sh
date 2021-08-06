46a508f12fed0013c58a08b3cbdf77cb3e7794fe
Jumperkables <tom.winterbottom@msn.com>
2021-08-06 13:42:35 +0100

    scripts ready to run on NCC

--- scripts/runs/tasks/CNN/bounces_2d/149-2dB_200-regress_59-1_k-3_d-3_lr-1e-6_sl1.sh
+++ scripts/runs/tasks/CNN/bounces_2d/149-2dB_200-regress_59-1_k-3_d-3_lr-1e-6_sl1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --ntasks 6
 #SBATCH -p part0
-#SBATCH --job-name 106-2dB_200_bounces-regress_2d_59-1_k-3_d-3_lr-1e-6_sl1  
+#SBATCH --job-name 149-2dB_200_bounces-regress_2d_59-1_k-3_d-3_lr-1e-6_sl1  
 #SBATCH --gres gpu:1
-#SBATCH -o ../../../../../.results/106-2dB_200_bounces-regress_2d_59-1_k-3_d-3_lr-1e-6_sl1.out
+#SBATCH -o ../../../../../.results/149-2dB_200_bounces-regress_2d_59-1_k-3_d-3_lr-1e-6_sl1.out
 cd ../../../../..
 export PYTHONBREAKPOINT=ipdb.set_trace
 source python_venvs/vm/bin/activate
@@ -22,10 +22,10 @@ python test_tasks.py \
     --device 0 \
     --lr 1e-6 \
     --epoch 200 \
-    --jobname 106-2dB_200_bounces-regress_2d_59-1_k-3_d-3_lr-1e-6_sl1 \
+    --jobname 149-2dB_200_bounces-regress_2d_59-1_k-3_d-3_lr-1e-6_sl1 \
     --img_type greyscale \
     --model UpDown2D \
-    --model_path '.results/2dBouncingMG-y_59-1_k-3_d-3_lr-1e-4_sl1-mean-epoch=106.ckpt' \
+    --model_path '.results/2dBouncingMG-y_59-1_k-3_d-3_lr-1e-4_sl1-mean-epoch=149.ckpt' \
     --encoder_freeze \
     --linear_probes \
     --shuffle \
