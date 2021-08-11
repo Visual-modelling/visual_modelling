e3287dd9e7c2d6bbacf0c58d97df3168e38fde29
Jumperkables <tom.winterbottom@msn.com>
2021-08-11 19:56:48 +0100

    pt scripts ready

---
+++ scripts/runs/tasks/patch_transformer/moon/142-moon_200-regress_5-1_k-3_d-3_lr-1e-5_sl1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --qos short
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH -t 2-00:00
+#SBATCH --mem 16G
+#SBATCH -p res-gpu-small
+#SBATCH --job-name pt_142-moon_200_moon-regress_5-1_k-3_d-3_lr-1e-5_sl1  
+#SBATCH --gres gpu:1
+#SBATCH -o ../../../../../.results/pt_142-moon_200_moon-regress_5-1_k-3_d-3_lr-1e-5_sl1.out
+cd ../../../../..
+export PYTHONBREAKPOINT=ipdb.set_trace
+source python_venvs/vm/bin/activate
+
+# moon-regress Task
+python test_tasks.py \
+    --task moon-regress \
+    --dataset simulations  \
+    --dataset_path data/myphysicslab/Moon_10000 \
+    --bsz 64 \
+    --val_bsz 100 \
+    --num_workers 1 \
+    --in_no 5 \
+    --out_no 1 \
+    --depth 3 \
+    --device 0 \
+    --lr 1e-5 \
+    --epoch 400 \
+    --jobname pt_142-moon_200_moon-regress_5-1_k-3_d-3_lr-1e-5_sl1 \
+    --img_type greyscale \
+    --model PatchTrans \
+    --model_path '.results/pt_moon_5-1_k-3_d-3_lr-1e-4_sl1-mean-epoch=142.ckpt' \
+    --encoder_freeze \
+    --linear_probes \
+    --shuffle \
+    --wandb 
