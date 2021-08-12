6041ac944639208817fce8798a096061eca4ad3c
Jumperkables <tom.winterbottom@msn.com>
2021-08-13 00:05:59 +0100

    last few runs

--- scripts/runs/tasks/patch_transformer/bounces_2d/130-2dB_200-regress_59-1_k-3_d-3_sl1.sh
+++ scripts/runs/tasks/patch_transformer/bounces_2d/130-2dB_200-regress_59-1_k-3_d-3_sl1.sh
@@ -5,9 +5,9 @@
 #SBATCH -t 2-00:00
 #SBATCH --mem 16G
 #SBATCH -p res-gpu-small
-#SBATCH --job-name pt_12-2dB_200_bounces-regress_2d_59-1_k-3_d-3_sl1  
+#SBATCH --job-name pt_130-2dB_200_bounces-regress_2d_59-1_k-3_d-3_sl1  
 #SBATCH --gres gpu:1
-#SBATCH -o ../../../../../.results/pt_12-2dB_200_bounces-regress_2d_59-1_k-3_d-3_sl1.out
+#SBATCH -o ../../../../../.results/pt_130-2dB_200_bounces-regress_2d_59-1_k-3_d-3_sl1.out
 cd ../../../../..
 export PYTHONBREAKPOINT=ipdb.set_trace
 source python_venvs/vm/bin/activate
@@ -26,10 +26,10 @@ python test_tasks.py \
     --device 0 \
     --lr 3e-6 \
     --epoch 400 \
-    --jobname pt_12-2dB_200_bounces-regress_2d_59-1_k-3_d-3_sl1 \
+    --jobname pt_130-2dB_200_bounces-regress_2d_59-1_k-3_d-3_sl1 \
     --img_type greyscale \
     --model PatchTrans \
-    --model_path '.results/pt_2dBouncingMG-y_59-1_k-3_d-3_lr-1e-5_sl1-mean-epoch=12.ckpt' \
+    --model_path '.results/pt_2dBouncingMG-y_59-1_k-3_d-3_lr-6e-5_sl1-mean-epoch=130.ckpt' \
     --encoder_freeze \
     --linear_probes \
     --shuffle \
