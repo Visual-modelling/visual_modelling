9e876544ce82db0110251704e6947e0565f2723a
Jumperkables <tom.winterbottom@msn.com>
2021-08-17 15:00:05 +0100

    3d stuff

--- scripts/runs/modelling/CNN/3dBouncing/3dBouncing_5-1_k-3_d-3_lr-1e-3_ssim.sh
+++ scripts/runs/modelling/CNN/3dBouncing/3dBouncing_5-1_k-3_d-3_lr-1e-3_ssim.sh
@@ -5,16 +5,16 @@
 #SBATCH -t 2-00:00
 #SBATCH --mem 21G
 #SBATCH -p res-gpu-small
-#SBATCH --job-name 2dBouncingMG-y_5-1_k-3_d-3_lr-1e-2_ssim 
+#SBATCH --job-name 3dBouncing_5-1_k-3_d-3_lr-1e-3_ssim 
 #SBATCH --gres gpu:1
-#SBATCH -o ../../../../../.results/2dBouncingMG-y_5-1_k-3_d-3_lr-1e-2_ssim.out
+#SBATCH -o ../../../../../.results/3dBouncing_5-1_k-3_d-3_lr-1e-3_ssim.out
 cd ../../../../..
 source python_venvs/vm/bin/activate
 export PYTHONBREAKPOINT=ipdb.set_trace
 # Pretrain
 python VM_train.py \
     --dataset simulations \
-    --dataset_path data/2dBouncing/2dMultiGrav-Y_regen/raw \
+    --dataset_path data/3dBouncing/3dRegen \
     --split_condition tv_ratio:8-1-1 \
     --bsz 64 \
     --val_bsz 100 \
@@ -25,10 +25,10 @@ python VM_train.py \
     --krnl_size 3 \
     --padding 1 \
     --device 0 \
-    --lr 1e-2 \
+    --lr 1e-3 \
     --epoch 150 \
-    --n_gifs 12 \
-    --jobname 2dBouncingMG-y_5-1_k-3_d-3_lr-1e-2_ssim \
+    --n_gifs 20 \
+    --jobname 3dBouncing_5-1_k-3_d-3_lr-1e-3_ssim \
     --loss ssim \
     --reduction mean \
     --img_type greyscale \
