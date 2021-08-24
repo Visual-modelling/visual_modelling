902736b154ad708a296db4a85a7fc48f4256839d
Jumperkables <tom.winterbottom@msn.com>
2021-08-24 18:24:27 +0100

    3 runs ready to go to ncc

---
+++ scripts/runs/tasks/patch_transformer/blocks/random_200-regress_49-1_k-3_d-3_lr-8e-6.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --qos short
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH -t 2-00:00
+#SBATCH --mem 22G
+#SBATCH -p res-gpu-small
+#SBATCH --job-name pt_random_200_blocks-regress_49-1_k-3_d-3_lr-8e-6  
+#SBATCH --gres gpu:1
+#SBATCH -o ../../../../../.results/pt_random_200_blocks-regress_49-1_k-3_d-3_lr-8e-6.out
+cd ../../../../..
+export PYTHONBREAKPOINT=ipdb.set_trace
+source python_venvs/vm/bin/activate
+
+# blocks-regress Task
+python test_tasks.py \
+    --task blocks-regress \
+    --dataset simulations  \
+    --dataset_path data/myphysicslab/Blocks_10000 \
+    --bsz 64 \
+    --val_bsz 100 \
+    --num_workers 1 \
+    --in_no 49 \
+    --out_no 1 \
+    --depth 3 \
+    --device 0 \
+    --lr 8e-6 \
+    --epoch 200 \
+    --jobname pt_random_200_blocks-regress_49-1_k-3_d-3_lr-8e-6 \
+    --img_type greyscale \
+    --model PatchTrans \
+    --model_path '' \
+    --encoder_freeze \
+    --linear_probes \
+    --shuffle \
+    --wandb 
--- scripts/runs/tasks/patch_transformer/roller/random_200-regress_5-1_k-3_d-3_lr-6e-6.sh
+++ scripts/runs/tasks/patch_transformer/roller/random_200-regress_5-1_k-3_d-3_lr-6e-6.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-#SBATCH --ntasks 6
-#SBATCH -p part0
-#SBATCH --job-name pt_random_200_roller-regress_5-1_k-3_d-3_lr-3e-6  
+#SBATCH --qos short
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH -t 2-00:00
+#SBATCH --mem 22G
+#SBATCH -p res-gpu-small
+#SBATCH --job-name pt_random_200_roller-regress_5-1_k-3_d-3_lr-6e-6  
 #SBATCH --gres gpu:1
-#SBATCH -o ../../../../../.results/pt_random_200_roller-regress_5-1_k-3_d-3_lr-3e-6.out
+#SBATCH -o ../../../../../.results/pt_random_200_roller-regress_5-1_k-3_d-3_lr-6e-6.out
 cd ../../../../..
 export PYTHONBREAKPOINT=ipdb.set_trace
 source python_venvs/vm/bin/activate
@@ -20,14 +24,13 @@ python test_tasks.py \
     --out_no 1 \
     --depth 3 \
     --device 0 \
-    --lr 3e-6 \
+    --lr 6e-6 \
     --epoch 200 \
-    --jobname pt_random_200_roller-regress_5-1_k-3_d-3_lr-3e-6 \
+    --jobname pt_random_200_roller-regress_5-1_k-3_d-3_lr-6e-6 \
     --img_type greyscale \
     --model PatchTrans \
     --model_path '' \
     --encoder_freeze \
     --linear_probes \
     --shuffle \
-    --disable_preload \
     --wandb 
