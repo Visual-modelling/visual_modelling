c36c65f9b132171e13c5483b397ff1520aa565e0
Daniel Kluvanec <Daniel.Kluvanec@Durham.ac.uk>
2021-08-25 15:47:16 +0100

    Updated scripts for image transformer test tasks

---
+++ scripts/runs/tasks/image_transformer/mmnist/mmnist_mnist_transformer_untrained_lr1e-6_021.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --qos short
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH -t 2-00:00
+#SBATCH --mem 28G
+#SBATCH -p res-gpu-small
+#SBATCH --job-name mmnist_mnist_transformer_untrained_021.sh
+#SBATCH --gres gpu:1
+#SBATCH -o ../../../../../.results/mmnist_mnist_transformer_untrained_021.out
+cd ../../../../..
+source python_venvs/vm/bin/activate
+export PYTHONBREAKPOINT=ipdb.set_trace
+python test_tasks.py \
+    --task mnist \
+    --dataset_path data/moving_mnist/1_2_3 \
+    --model_path '' \
+    --jobname mmnist_mnist_transformer_untrained_021 \
+    --dataset simulations \
+    --split_condition tv_ratio:8-1-1 \
+    --bsz 64 \
+    --val_bsz 64 \
+    --num_workers 1 \
+    --in_no 5 \
+    --out_no 1 \
+    --device 0 \
+    --epoch 500 \
+    --early_stopping 10 \
+    --img_type greyscale \
+    --shuffle \
+    --wandb \
+    --model image_transformer \
+    --d_model 4096 \
+    --n_layers 2 \
+    --nhead 4 \
+    --dim_feedforward 4096 \
+    --dropout 0.0 \
+    --pixel_regression_layers 1 \
+    --norm_layer layer_norm \
+    --output_activation hardsigmoid-256 \
+    --pos_encoder add_runtime \
+    --mask \
+    --lr 1e-6 \
