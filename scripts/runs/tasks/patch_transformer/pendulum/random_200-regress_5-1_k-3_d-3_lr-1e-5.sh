fb92b98b5ba3d7a41600f6c8423702b0dec70195
Jumperkables <tom.winterbottom@msn.com>
2021-08-25 16:05:33 +0100

    random scripts prepared

---
+++ scripts/runs/tasks/patch_transformer/pendulum/random_200-regress_5-1_k-3_d-3_lr-1e-5.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --qos short
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH -t 2-00:00
+#SBATCH --mem 22G
+#SBATCH -p res-gpu-small
+#SBATCH --job-name pt_random_200_pendulum-regress_5-1_k-3_d-3_lr-1e-5  
+#SBATCH --gres gpu:1
+#SBATCH -o ../../../../../.results/pt_random_200_pendulum-regress_5-1_k-3_d-3_lr-1e-5.out
+cd ../../../../..
+export PYTHONBREAKPOINT=ipdb.set_trace
+source python_venvs/vm/bin/activate
+
+# pendulum Task
+python test_tasks.py \
+    --task pendulum-regress \
+    --dataset simulations  \
+    --dataset_path data/myphysicslab/Pendulum_10000 \
+    --bsz 64 \
+    --val_bsz 100 \
+    --num_workers 1 \
+    --in_no 5 \
+    --out_no 1 \
+    --depth 3 \
+    --device 0 \
+    --lr 1e-5 \
+    --epoch 200 \
+    --jobname pt_random_200_pendulum-regress_5-1_k-3_d-3_lr-1e-5 \
+    --img_type greyscale \
+    --model PatchTrans \
+    --model_path '' \
+    --encoder_freeze \
+    --linear_probes \
+    --shuffle \
+    --wandb 
