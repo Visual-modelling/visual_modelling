56f84b60731af0204ee2b95fc0a78a86d668db09
Jumperkables <tom.winterbottom@msn.com>
2021-08-26 13:06:11 +0100

    experimental reruns ready

--- scripts/runs/tasks/CNN/moon/127-moon_200-regress_5-1_k-3_d-3_ssim.sh
+++ scripts/runs/tasks/CNN/moon/127-moon_200-regress_5-1_k-3_d-3_ssim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --ntasks 6
 #SBATCH -p part0
-#SBATCH --job-name 1e-5_127-moon_200_moon-regress_5-1_k-3_d-3_ssim  
+#SBATCH --job-name 2e-5_127-moon_200_moon-regress_5-1_k-3_d-3_ssim  
 #SBATCH --gres gpu:1
-#SBATCH -o ../../../../../.results/1e-5_127-moon_200_moon-regress_5-1_k-3_d-3_ssim.out
+#SBATCH -o ../../../../../.results/2e-5_127-moon_200_moon-regress_5-1_k-3_d-3_ssim.out
 cd ../../../../..
 export PYTHONBREAKPOINT=ipdb.set_trace
 source python_venvs/vm/bin/activate
@@ -20,9 +20,9 @@ python test_tasks.py \
     --out_no 1 \
     --depth 3 \
     --device 0 \
-    --lr 1e-5 \
+    --lr 2e-5 \
     --epoch 400 \
-    --jobname 1e-5_127-moon_200_moon-regress_5-1_k-3_d-3_ssim \
+    --jobname 2e-5_127-moon_200_moon-regress_5-1_k-3_d-3_ssim \
     --img_type greyscale \
     --model UpDown2D \
     --model_path '.results/moon_5-1_k-3_d-3_lr-1e-3_ssim-epoch=127.ckpt' \
