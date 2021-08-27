3b5eb0d9ad72fc5b1e37642eb9ac99f87ecc539f
Daniel Kluvanec <Daniel.Kluvanec@Durham.ac.uk>
2021-08-27 14:14:26 +0100

    Updated scripts for image transformer test tasks

---
+++ scripts/runs/tasks/image_transformer/roller/roller_roller-regress_transformer_lr1e-7_sl1_021.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#SBATCH --qos short
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH -t 2-00:00
+#SBATCH --mem 28G
+#SBATCH -p res-gpu-small
+#SBATCH --job-name roller_roller-regress_transformer_lr1e-7_sl1_021.sh
+#SBATCH --gres gpu:1
+#SBATCH -o ../../../../../.results/roller_roller-regress_transformer_lr1e-7_sl1_021.out
+cd ../../../../..
+source python_venvs/vm/bin/activate
+export PYTHONBREAKPOINT=ipdb.set_trace
+python test_tasks.py \
+    --task roller-regress \
+    --dataset_path data/myphysicslab/RollerFlight_10000_bigger \
+    --model_path .best_runs/roller_transformer_lr1e-5_sl1_021-epoch=138.ckpt \
+    --linear_probes \
+    --encoder_freeze \
+    --jobname roller_roller-regress_transformer_lr1e-7_sl1_021 \
+    --dataset simulations \
+    --split_condition tv_ratio:8-1-1 \
+    --bsz 64 \
+    --val_bsz 64 \
+    --num_workers 1 \
+    --in_no 5 \
+    --out_no 1 \
+    --device 0 \
+    --epoch 500 \
+    --early_stopping 10 \
+    --img_type greyscale \
+    --shuffle \
+    --wandb \
+    --model image_transformer \
+    --d_model 4096 \
+    --n_layers 2 \
+    --nhead 4 \
+    --dim_feedforward 4096 \
+    --dropout 0.0 \
+    --pixel_regression_layers 1 \
+    --norm_layer layer_norm \
+    --output_activation hardsigmoid-256 \
+    --pos_encoder add_runtime \
+    --mask \
+    --lr 1e-7 \
